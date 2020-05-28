63b3daceb5e628b77b657a1691a71286af5d1b07
Tatiana Mattioly Valente <tatiana.valente@dtidigital.com.br>
2020-05-28 18:02:04 -0300

    drag and drop works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset'
+import { DragDropContext } from 'react-beautiful-dnd'
+import initialData from './inicial-data';
+import Column from './column';
 
-const App = () => 'Hello Word';
+class App extends React.Component {
+  state = initialData;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  onDragEnd = result => {
+    // todo
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column
+            key={column.id}
+            column={column}
+            tasks={tasks}
+          />
+        })}
+      </DragDropContext>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/task.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
+
+const Container = styled.div`
+border: 1px solid lightgrey;
+padding: 8px;
+border-radius: 2px;
+margin-bottom:8px;
+`;
+
+export default class Task extends React.Component {
+
+    render() {
+        return (
+            <Draggable draggableId={this.props.task.id} index={this.props.index}>
+                {provided => (
+                    <Container
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                    >
+                        {this.props.task.content}
+                    </Container>
+                )}
+            </Draggable>
+        );
+    }
+}
